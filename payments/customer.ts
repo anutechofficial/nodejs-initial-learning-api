1e1182676dedbdaeb586c53eae772db9b70432d0
Anurag Yadav <anutechofficial@gmail.com>
2023-10-16 13:03:23 +0530

    completed stripe customer payment process

--- payments/customer.ts
+++ payments/customer.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Stripe from "stripe";
 import { config } from "dotenv";
+// import { Request, Response } from 'aws-sdk';
 config();
 const secretKey=process.env.STRIPE_SECRET_KEY as string;
 const stripe=new Stripe(secretKey, {apiVersion:'2023-08-16'});
@@ -22,27 +23,47 @@ export const addNewCard = async(req:Request,res:Response)=>{
     try {
         const {
             customer_id,
-            card_Name,
-            card_ExpYear,
-            card_ExpMonth,
-            card_Number,
-            card_CVC,
+            // card_Name,
+            // card_ExpYear,
+            // card_ExpMonth,
+            // card_Number,
+            // card_CVC,
+
         } = req.body;
 
-        const card_token = await stripe.tokens.create({
-            card:{
-                name: card_Name,
-                number: card_Number,
-                exp_year: card_ExpYear,
-                exp_month: card_ExpMonth,
-                cvc: card_CVC
-            }
-        });
-        const card = await stripe.customers.createSource(customer_id, {
-            source: `${card_token.id}`
-        });
+        // const card_token = await stripe.tokens.create({
+        //     card:{
+        //         name: card_Name,
+        //         number: '4242424242424242',
+        //         exp_year: card_ExpYear,
+        //         exp_month: card_ExpMonth,
+        //         cvc: card_CVC
+        //     }
+        // });
+        // const card = await stripe.customers.createSource(customer_id, {
+        //     source: `${card_token.id}`
+        // });
+        const card = await stripe.customers.createSource(
+            customer_id,
+            {source: 'tok_amex'}
+          );
         res.status(200).send({ card: card.id });
     } catch (error:any) {
         res.status(400).send({success:false,msg:error.message});
     }
+}
+
+export const createCharges= async (req:Request, res:Response)=>{
+    try {
+        const createdCharges= await stripe.charges.create({
+            receipt_email:req.body.email,
+            amount:parseInt(req.body.amount)*100,
+            currency:'USD',
+            // card:req.body.card_id,
+            customer: req.body.customer_id,
+        });
+        res.status(200).send(createdCharges);
+    } catch (error:any) {
+        res.status(400).send({success:false, msg: error.message})
+    }     
 }
--- routes/stripe.ts
+++ routes/stripe.ts
@@ -1,4 +1,4 @@
-import {createCustomer, addNewCard} from "../payments/customer";
+import {createCustomer, addNewCard, createCharges} from "../payments/customer";
 
 import  express  from "express";
 
@@ -6,6 +6,7 @@ const router=express.Router();
 
 router.post('/createCustomer', createCustomer);
 router.post('/addCard', addNewCard);
+router.post('/charges', createCharges);
 
 
 export default router;
